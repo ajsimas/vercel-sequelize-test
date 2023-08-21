f4c0bec1c10190a02654eddddd8e9543cca30842
Austin Simas <austin@simas.io>
2023-08-21 13:15:02 -0700

    add form / handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,11 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex min-h-screen flex-col items-center justify-center">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,51 @@
+'use client';
+import {useState} from 'react';
+
 export default function Home() {
-  return <></>;
+  const [name, setName] = useState('');
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(name);
+    const response = await fetch('/api/name', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({name}),
+    });
+
+    if (!response.ok) {
+      console.log('error');
+    }
+
+    const data = await response.json();
+    console.log(data);
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
+  }
+
+  return (
+    <div>
+      <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
+        <div className="flex flex-col">
+          <label className="" htmlFor="name">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="border border-blue-800"
+            onChange={handleChange}
+          />
+        </div>
+        <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
