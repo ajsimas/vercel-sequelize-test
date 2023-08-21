7fb5432795445c8483170c542a3e549139c0024f
Austin Simas <austin@simas.io>
2023-08-21 16:17:09 -0700

    remove db call for testing

--- src/app/api/name/route.ts
+++ src/app/api/name/route.ts
@@ -1,9 +1,15 @@
 import {NextResponse} from 'next/server';
-import {UserModel} from '@/db/models';
-import {v4 as uuidv4} from 'uuid';
+// import {UserModel} from '@/db/models';
+// import {v4 as uuidv4} from 'uuid';
+
+// export async function POST(req: Request) {
+//   const {name} = await req.json();
+//   const userModel = await UserModel.create({name, uuid: uuidv4()});
+// eslint-disable-next-line max-len
+//   return NextResponse.json({greeting: `Hello ${userModel.dataValues.uuid}!`});
+// }
 
 export async function POST(req: Request) {
   const {name} = await req.json();
-  const userModel = await UserModel.create({name, uuid: uuidv4()});
-  return NextResponse.json({greeting: `Hello ${userModel.dataValues.uuid}!`});
+  return NextResponse.json({greeting: `Hello ${name}!`});
 }
