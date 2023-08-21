179dfd4293c27b4012e8ca933c3a25ee7235c7bb
Austin Simas <austin@simas.io>
2023-08-21 15:53:38 -0700

    configure route

--- src/app/api/name/route.ts
+++ src/app/api/name/route.ts
@@ -1,7 +1,9 @@
 import {NextResponse} from 'next/server';
+import {UserModel} from '@/db/models';
+import {v4 as uuidv4} from 'uuid';
 
 export async function POST(req: Request) {
   const {name} = await req.json();
-  console.log('debug1', name);
-  return NextResponse.json({greeting: `Hello ${name}!`});
+  const userModel = await UserModel.create({name, uuid: uuidv4()});
+  return NextResponse.json({greeting: `Hello ${userModel.dataValues.uuid}!`});
 }
