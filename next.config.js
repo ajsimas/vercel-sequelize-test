fcecff6a0fca7792fe5ff6f41f0bcfd7f3fd6969
Austin Simas <austin@simas.io>
2023-08-21 15:53:27 -0700

    sequelize hack

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, {isServer}) => {
+    if (isServer) {
+      config.externals = [
+        ...config.externals,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        function ({context, request}, callback) {
+          if (request === 'pg-hstore') {
+            return callback(null, 'commonjs ' + request);
+          }
+          callback();
+        },
+      ];
+    }
+    config.ignoreWarnings = [
+      {
+        module: /sequelize/,
+        message:
+          /Critical dependency: the request of a dependency is an expression/,
+      },
+    ];
+    return config;
+  },
+};
