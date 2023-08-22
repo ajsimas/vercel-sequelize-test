2bdcc04792256b8e66813d48321c3ffb76db19c3
Austin Simas <austin@simas.io>
2023-08-22 09:24:37 -0700

    remove edge

--- src/app/api/name/route.ts
+++ src/app/api/name/route.ts
@@ -2,8 +2,6 @@ import {NextRequest, NextResponse} from 'next/server';
 import {UserModel} from '@/db/models';
 import {v4 as uuidv4} from 'uuid';
 
-export const runtime = 'edge';
-
 export async function POST(req: NextRequest) {
   const {name} = await req.json();
   const userModel = await UserModel.create({name, uuid: uuidv4()});
