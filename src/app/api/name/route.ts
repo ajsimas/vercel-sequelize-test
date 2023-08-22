91f20b46b75ca8c9bd074ba117959568f068d0cc
Austin Simas <austin@simas.io>
2023-08-21 20:41:33 -0700

    enable route with db call

--- src/app/api/name/route.ts
+++ src/app/api/name/route.ts
@@ -1,17 +1,16 @@
 import {NextRequest, NextResponse} from 'next/server';
-// import {UserModel} from '@/db/models';
-// import {v4 as uuidv4} from 'uuid';
-
-// export async function POST(req: Request) {
-//   const {name} = await req.json();
-//   const userModel = await UserModel.create({name, uuid: uuidv4()});
-// eslint-disable-next-line max-len
-//   return NextResponse.json({greeting: `Hello ${userModel.dataValues.uuid}!`});
-// }
+import {UserModel} from '@/db/models';
+import {v4 as uuidv4} from 'uuid';
 
 export const runtime = 'edge';
 
 export async function POST(req: NextRequest) {
   const {name} = await req.json();
-  return NextResponse.json({greeting: `Hello ${name}!`});
+  const userModel = await UserModel.create({name, uuid: uuidv4()});
+  return NextResponse.json({greeting: `Hello ${userModel.dataValues.uuid}!`});
 }
+
+// export async function POST(req: NextRequest) {
+//   const {name} = await req.json();
+//   return NextResponse.json({greeting: `Hello ${name}!`});
+// }
